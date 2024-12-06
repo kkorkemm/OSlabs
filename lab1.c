5ada239752931f72c530149a4ea1debcf3b3ce06
kkorkemm <zhardemova2012@mail.ru>
2024-12-07 00:07:15 +0700

    lab1

---
+++ lab1.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h> 
+
+pthread_cond_t cond1 = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+int ready = 0;
+
+// Поставщик
+void provide() {
+    while (1) {
+        // Задержка в 1 секунду
+        sleep(1);
+
+        // Блокируем мьютекс для безопасного доступа к разделяемому ресурсу
+        pthread_mutex_lock(&lock);
+        
+        if (ready == 1)
+        {
+            pthread_mutex_unlock(&lock);
+            continue;
+        }
+
+        ready = 1;
+        printf("provided\n");
+        pthread_cond_signal(&cond1);
+        pthread_mutex_unlock(&lock);
+    }
+}
+
+// Потребитель
+void consume() {
+    while (1) {
+        pthread_mutex_lock(&lock);
+
+        // Ожидаем события с минимальным потреблением процессорного времени
+        while (!ready) {
+            pthread_cond_wait(&cond1, &lock);
+            printf("awoke\n");
+        }
+
+        // Событие обработано
+        printf("consumed\n");
+        ready = 0;
+
+        pthread_mutex_unlock(&lock);
+    }
+}
+
+int main() {
+    pthread_t producer_thread, consumer_thread;
+
+    // Создаем потоки
+    pthread_create(&producer_thread, NULL, provide, NULL);
+    pthread_create(&consumer_thread, NULL, consume, NULL);
+
+    // Ждем завершения потоков (в данном случае, они бесконечные)
+    pthread_join(producer_thread, NULL);
+    pthread_join(consumer_thread, NULL);
+
+    // Освобождаем ресурсы
+    pthread_mutex_destroy(&lock);
+    pthread_cond_destroy(&cond1);
+
+    return 0;
+}
